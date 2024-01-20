6452ccb477c3127d039e454d5f17f219babb9ac7
Adam Risser <arisser@gmail.com>
2024-01-20 15:48:00 -0500

    switching to summary based memory

--- tchat/main.py
+++ tchat/main.py
@@ -1,17 +1,23 @@
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory, ConversationSummaryMemory
 
 from dotenv import load_dotenv
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(verbose=True)
 
-memory = ConversationBufferMemory(
+# memory = ConversationBufferMemory(
+#   memory_key="messages", 
+#   return_messages=True,
+#   chat_memory=FileChatMessageHistory("messages.json")
+# )
+
+memory = ConversationSummaryMemory(
   memory_key="messages", 
   return_messages=True,
-  chat_memory=FileChatMessageHistory("messages.json")
+  llm=chat
 )
 
 prompt = ChatPromptTemplate(
@@ -25,7 +31,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
   llm=chat,
   prompt=prompt,
-  memory=memory
+  memory=memory,
+  verbose=True
 )
 
 while True:
